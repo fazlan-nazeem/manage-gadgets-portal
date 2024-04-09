82ef07f5e3db567183095a10b1721592f198889f
Fazlan Nazeem <fazlan.pera@gmail.com>
2024-04-09 01:25:19 +0100

    fix mount path

--- public/config.js
+++
@@ -1,4 +0,0 @@
-
-window.configs = {
-  apiUrl: '/choreo-apis/ygns/managegadgetsapi/manage-gadget-api-bfc/v1.0',
-};
--- public/env-config.js
+++
@@ -1,6 +0,0 @@
-window.env = {
-  SIGN_IN_REDIRECT_URL: 'http://localhost:3000/app/dashboard',
-  SIGN_OUT_REDIRECT_URL: 'http://localhost:3000/login',
-  CLIENT_ID: 'RdRCqz8e1wmQPmCy1EYcI19twr8a'
-};
-
