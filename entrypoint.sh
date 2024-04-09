e3fd96ce9b3286d8aab262d9af5508fa3d01e3a3
Fazlan Nazeem <fazlan.pera@gmail.com>
2024-04-09 01:41:15 +0100

    modify index.html

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 echo "SIGN_IN_REDIRECT_URL=${SIGN_IN_REDIRECT_URL}"
 
 # Replace environment variables in files
-envsubst < /usr/src/app/src/template.env.js > /usr/src/app/public/configs/env-config.js
+envsubst < /usr/src/app/src/template.env.js > /usr/src/app/public/configs/asgardeo/env-config.js
 
 # Execute the CMD from the Dockerfile, e.g., starting your web server
 exec "$@"
